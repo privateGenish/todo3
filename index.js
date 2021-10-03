51f8638b81fb33e49e3da1d2402c5eb14890a628
therealgenish <the.real.genish@gmail.com>
2021-10-03 15:42:53 +0300

    fixed port

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 console.log("Starting...");
 const app = require("./app");
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(3000, () => console.log("listening at http://localhost:3000/api"));
